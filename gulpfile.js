1a52706b8b7ed7e11cd0a989fcc2deb662dc7fb4
Inthrayuth Mahaphol <zign.int@gmail.com>
2016-05-24 10:45:50 -0700

    run (npm install gulp-concat --save-dev).

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,16 @@
 var gulp = require('gulp');
 var browserify = require('browserify');
 var source = require('vinyl-source-stream');
+var concat = require('gulp-concat');
 
-gulp.task('jsBrowserify', function() {
-  return browserify({ entries: ['./'] })
+gulp.task('concatInterface', function() {
+  return gulp.src(['./js/browser.js'])
+  .pipe(concat('allConcat.js'))
+  .pipe(gulp.dest('./tmp'));
+});
+
+gulp.task('jsBrowserify', ['concatInterface'] , function() {
+  return browserify({ entries: ['./tmp/allConcat.js'] })
     .bundle()
     .pipe(source('app.js'))
     .pipe(gulp.dest('./build/js'));
